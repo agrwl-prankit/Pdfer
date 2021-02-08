a8c43d4f02c4596b302fd94bc9078853a412f5f1
agrwl-prankit <agrwnlprankit77@gmail.com>
2021-02-09 00:02:55 +0530

    Initial commit

---
+++ app/src/main/java/com/prankit/pdfer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.prankit.pdfer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
